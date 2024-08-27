f6baa20ff47c4fa5fda3be7a2d9fe81bd43d3962
XOMAK12 <nnikitaivanov202@gmail.com>
2024-08-28 04:46:17 +0600

    hw7

---
+++ src/pages/contactsPage/ContactsPage.js
@@ -0,0 +1,139 @@
+import { Container, TextField, Button, List, ListItem, ListItemText, IconButton, Dialog, DialogTitle, DialogContent, DialogActions, MenuItem, Select } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addContact, deleteContact, editContact } from "../../store/ContactsSlice";
+import BrushIcon from '@mui/icons-material/Brush';
+import DeleteForever from "@mui/icons-material/DeleteForever";
+
+const ContactsPage = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [editId, setEditId] = useState(null);
+    const [editName, setEditName] = useState('');
+    const [editEmail, setEditEmail] = useState('');
+    const [open, setOpen] = useState(false);
+    const [filter, setFilter] = useState('id');
+
+    const dispatch = useDispatch();
+    const contacts = useSelector(state => state.contactsReducer.contacts);
+
+    const handleAddContact = () => {
+        if (name && email) {
+            dispatch(addContact({ name, email }));
+            setName('');
+            setEmail('');
+        }
+    };
+
+    const handleEditContact = (contact) => {
+        setEditId(contact.id);
+        setEditName(contact.name);
+        setEditEmail(contact.email);
+        setOpen(true);
+    };
+
+    const handleSaveEdit = () => {
+        dispatch(editContact({ id: editId, name: editName, email: editEmail }));
+        setOpen(false);
+    };
+
+    const filteredContacts = () => {
+        switch (filter) {
+            case 'id':
+                return [...contacts].sort((a, b) => a.id - b.id);
+            case 'date':
+                return [...contacts].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+            case 'alphabet':
+                return [...contacts].sort((a, b) => a.name.localeCompare(b.name));
+            default:
+                return contacts;
+        }
+    };
+
+    return (
+        <Container>
+            <h1>Contacts</h1>
+            <TextField
+                label="Name"
+                variant="outlined"
+                required
+                value={name}
+                onChange={e => setName(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Email"
+                variant="outlined"
+                required
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <Button variant="outlined" color="primary" onClick={handleAddContact}>
+                Add Contact
+            </Button>
+
+            <Select
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+                displayEmpty
+                fullWidth
+                margin="normal"
+            >
+                <MenuItem value="id">Sort by ID</MenuItem>
+                <MenuItem value="date">Sort by Date</MenuItem>
+                <MenuItem value="alphabet">Sort by Alphabet</MenuItem>
+            </Select>
+
+            <List>
+                {filteredContacts().map(contact => (
+                    <ListItem key={contact.id}>
+                        <ListItemText primary={contact.name} secondary={`${contact.email} - ${new Date(contact.createdAt).toLocaleString()}`} />
+                        <IconButton edge="end" onClick={() => handleEditContact(contact)} >
+                            <BrushIcon />
+                        </IconButton>
+                        <IconButton edge="end" onClick={() => dispatch(deleteContact(contact.id))} >
+                            <DeleteForever />
+                        </IconButton>
+                    </ListItem>
+                ))}
+            </List>
+
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>Edit Contact</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        label="Name"
+                        variant="outlined"
+                        required
+                        value={editName}
+                        onChange={e => setEditName(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        label="Email"
+                        variant="outlined"
+                        required
+                        value={editEmail}
+                        onChange={e => setEditEmail(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSaveEdit} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Container>
+    );
+};
+
+export default ContactsPage;
